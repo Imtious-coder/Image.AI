de19fa119165c0afaa4566d9aa3c17845ad47660
Imtious-Coder <iarafat386@gmail.com>
2024-03-07 10:45:09 +0600

    Home page content, style, & animation completed.

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import CustomButton from "./CustomButton.jsx";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export {
+  AIPicker,
+  ColorPicker, CustomButton, FilePicker,
+  Tab
+};
+
